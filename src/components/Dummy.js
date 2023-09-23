5c19f7437ae0ce0830058b7b729b3ef949a1108a
= <=>
2023-09-23 20:27:01 +0530

    Commit Changes

---
+++ src/components/Dummy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dummy() {
+  return (
+    <div>dummy</div>
+  )
+}
+
+export default Dummy
---
+++ src/components/Login.js
@@ -0,0 +1,188 @@
+import React, { useState } from "react";
+import css from "./style.css";
+import axios from "axios";
+import ForgotPassword from "./ForgotPassword";
+import GoogleLogin from "react-google-login";
+function Login(props) {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginData((preValue) => {
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  }
+  const responseSuccessGoogle = async (response) => {
+    console.log(response);
+
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/googlelogin",
+        {
+          tokenId: response.tokenId,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(data);
+      if (data) {
+        if (data.errors) {
+          // handel error...
+          console.log("In corrcet credentials.");
+        } else {
+          return props.onBack("profile");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleClick = async (event) => {
+    event.preventDefault();
+    // if(loginData.email.length ===0 || loginData.password.length===0){
+    //     alert("Fields should not be empty");
+    //     return;
+    // }
+
+    const { email, password } = loginData;
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(data);
+      if (data) {
+        if (data.errors) {
+          // handel error...
+          console.log("In corrcet credentials.");
+          alert("Incorrcet credentials");
+        } else {
+          return props.onChecked("profile");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return props.onBack("profile");
+  };
+  const responseErrorGoogle = (response) => {
+    console.log(response);
+  };
+  /*******HF**********/
+  const [page, setPage] = useState({
+    oldPassword: true,
+    newPassword: false,
+  });
+  function handlePassword() {
+    setPage(() => {
+      return {
+        oldPassword: false,
+        newPassword: true,
+      };
+    });
+  }
+
+  // return(
+  //   <>
+  //   <form className="form">
+  //     <div className="boxLog">
+  //         <img src="https://qph.fs.quoracdn.net/main-thumb-1278318002-200-ydzfegagslcexelzgsnplcklfkienzfr.jpeg" alt="img" className="avatarLog" />
+  //         <h2>Get Started</h2>
+  //         <div className="login">
+  //             <h2>Email</h2>
+  //             <input className="loginbt" type="email" placeholder="Enter Email" name="email" onChange={handleChange} />
+  //         </div>
+  //         <div className="login">
+  //             <h2>Password</h2>
+  //             <input className="loginbt" type="password" placeholder="Enter Password" name="password" onChange={handleChange} />
+  //         </div>
+  //         <div className="login">
+  //             <input type="button" value="Login" onClick={handleClick} />
+  //         </div>
+  //         <div>
+  //       <GoogleLogin className="setlog1"
+  //         clientId="634193116808-mhg7vbt3hph1bg2sb0sfia6skijf3o71.apps.googleusercontent.com"
+  //         buttonText="Login with google"
+  //         onSuccess={responseSuccessGoogle}
+  //         onFailure={responseErrorGoogle}
+  //         cookiePolicy={'single_host_origin'}
+  //     />
+  //       </div>
+  //     </div>
+  //     </form>
+  //   </>
+  // );
+
+  return (
+    <>
+      {page.oldPassword ? (
+        <form className="form">
+          <div className="boxLog">
+            <img
+              src="https://qph.fs.quoracdn.net/main-thumb-1278318002-200-ydzfegagslcexelzgsnplcklfkienzfr.jpeg"
+              alt="img"
+              className="avatarLog"
+            />
+            <h2>Get Started</h2>
+            <div className="login">
+              <h2>Email</h2>
+              <input
+                className="loginbt"
+                type="email"
+                placeholder="Enter Email"
+                name="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login">
+              <h2>Password</h2>
+              <input
+                className="loginbt"
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login">
+              <input type="button" value="Login" onClick={handleClick} />
+            </div>
+            <div>
+              <GoogleLogin
+                className="setlog1"
+                clientId="634193116808-mhg7vbt3hph1bg2sb0sfia6skijf3o71.apps.googleusercontent.com"
+                buttonText="Login with google"
+                onSuccess={responseSuccessGoogle}
+                onFailure={responseErrorGoogle}
+                cookiePolicy={"single_host_origin"}
+              />
+            </div>
+            <div className="login">
+              <input
+                type="button"
+                value="Forgot Password"
+                onClick={handlePassword}
+              />
+            </div>
+          </div>
+        </form>
+      ) : null}
+      {page.newPassword ? <ForgotPassword /> : null}
+    </>
+  );
+}
+
+export default Login;
